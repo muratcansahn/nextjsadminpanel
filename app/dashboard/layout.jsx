d02990130e19dbe047bd2c01517dce2c91536f13
muratcansahn <muratcansahin@Murat-MacBook-Air.local>
2024-03-10 20:35:16 +0300

    feat:add sidebar

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Navbar from '../ui/dashboard/navbar/navbar'
 import Sidebar from '../ui/dashboard/sidebar/sidebar'
+import styles from '../ui/dashboard/dashboard.module.css'
 
 const Layout = ({children}) => {
   return (
-  <div>
-    <div>
+  <div className={styles.container}>
+    <div className={styles.menu}>
     <Sidebar />
 
     </div>
-    <div>
+    <div className={styles.content}>
       <Navbar />
-{children}
+    {children}
     </div>
 
   </div>  )
